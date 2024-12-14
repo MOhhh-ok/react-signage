1b196e633048cffdf69ecb55a250ce0b7730e5fa
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2024-12-14 17:46:44 +0900

    -

--- docs-src/src/demoItems.tsx
+++ docs-src/src/demoItems.tsx
@@ -1,22 +1,22 @@
-import { SignageItem } from "@masa-dev/react-signage";
+import { BASE_PATH } from '../config';
 
 export const demoItems = {
     img_infinite: {
         type: 'image',
-        src: "https://images-assets.nasa.gov/image/iss070e044474/iss070e044474~orig.jpg",
+        src: BASE_PATH + '/img1.jpg',
         second: 0,
     },
     img_short: {
         type: 'image',
-        src: "https://images-assets.nasa.gov/image/iss070e044474/iss070e044474~orig.jpg",
+        src: BASE_PATH + '/img2.jpt',
         second: 1,
     },
     video1: {
         type: 'video',
-        src: "/video3.mp4"
+        src: BASE_PATH + "/video3.mp4"
     },
     video2: {
         type: 'video',
-        src: "/video4.mp4"
+        src: BASE_PATH + "/video4.mp4"
     },
 } as const;
--- docs-src/vite.config.ts
+++ docs-src/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { BASE_PATH } from './config';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/react-signage/',
+    base: BASE_PATH,
     build: {
         outDir: '../docs',
     },
